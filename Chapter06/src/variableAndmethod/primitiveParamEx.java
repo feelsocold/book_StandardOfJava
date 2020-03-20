6b7f819ab9704fc0e3bbb957a3c5a62d87a500cf
feelsocold <feelsocoold@gmail.com>
2020-03-20 23:56:53 +0900

    CH06.3.8 참조형

---
+++ Chapter06/src/variableAndmethod/primitiveParamEx.java
@@ -0,0 +1,26 @@
+package variableAndmethod;
+
+class Data1 { int x; }
+
+public class primitiveParamEx {
+
+	public static void main(String[] args) {
+		
+		Data d = new Data();
+		d.x = 10;
+		System.out.println("main() : x = " + d.x);
+		
+		change(d);
+		System.out.println("AFTER Change(d.x)");
+		System.out.println("main() : x = " + d.x);
+		
+	}
+	
+	static void change(Data d) {		// 참조형 매개변수
+		d.x = 1000;
+		System.out.println("change() : x = " + d.x);
+	}
+
+}
+
+
